8209851836211d2240ca41bbcc9eda471451e2a6
Alexcsharp17 <aleshalekha@gmail.com>
2020-09-25 10:05:17 +0300

    Added basic entities

---
+++ src/CarStore/CarStore.DAL/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarStore.DAL.Entities
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public int CarID { get; set; }
+
+        public int PersonId { get; set; }
+    }
+}
---
+++ src/CarStore/CarStore.DAL/Entities/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarStore.DAL.Entities
+{
+    public class Person
+    {
+        public int PersonID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
